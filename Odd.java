d336a41c3d2b5a003ee366c0a0a3f2c985d51a33
Kaweesha-Malshani <537tharu@gmail.com>
2025-03-14 19:28:35 +0530

    question4

---
+++ Odd.java
@@ -0,0 +1,10 @@
+//4)print the odd numbers between 1 to 20
+
+public class Odd {
+    public static void main(String args[]) {
+for (int i = 1; i <= 20; i += 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+	}
+}
